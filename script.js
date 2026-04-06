bc81fc3d8f74ed6cf685601019fb6367eb2f1474
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-04-06 19:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const timeDisplay = document.querySelector(".time-display");
 const soundButtons = document.querySelectorAll(".sound-picker button");
 const timeButtons = document.querySelectorAll("#time-select button");
 
-let duration = 600; // default 10 min
+let duration = 600; // 10 minutes
 
 // PLAY / PAUSE
 playBtn.addEventListener("click", () => {
@@ -36,9 +36,17 @@ soundButtons.forEach(button => {
 // TIME SELECT
 timeButtons.forEach(button => {
   button.addEventListener("click", function () {
+
     if (this.id === "smaller-mins") duration = 120;
     if (this.id === "medium-mins") duration = 300;
     if (this.id === "long-mins") duration = 600;
+
+    // update display immediately
+    let minutes = Math.floor(duration / 60);
+    let seconds = Math.floor(duration % 60);
+
+    timeDisplay.textContent =
+      `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   });
 });
 
@@ -47,15 +55,16 @@ audio.ontimeupdate = () => {
   let currentTime = audio.currentTime;
   let remaining = duration - currentTime;
 
+  if (remaining <= 0) {
+    audio.pause();
+    video.pause();
+    audio.currentTime = 0;
+    return;
+  }
+
   let minutes = Math.floor(remaining / 60);
   let seconds = Math.floor(remaining % 60);
 
   timeDisplay.textContent =
     `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-
-  if (currentTime >= duration) {
-    audio.pause();
-    video.pause();
-    audio.currentTime = 0;
-  }
 };
